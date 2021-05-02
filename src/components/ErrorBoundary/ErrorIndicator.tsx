b749daa447dbf94e7f05cbd4c4d93a3fc9b0832d
romasrs <shaman.roman@tut.by>
2021-05-02 15:11:35 +0300

    feat: add some fixes

--- src/components/ErrorBoundary/ErrorIndicator.tsx
+++ src/components/ErrorBoundary/ErrorIndicator.tsx
@@ -5,8 +5,7 @@ import image404 from './error_404.png';
 
 const ErrorIndicator: React.FC = () => (
   <div className={styles.error}>
-    <img src={image404} alt="404" />
-    <h1>OOPS!</h1>
+    <img src={image404} alt="Error" />
     <span>Something went wrong!</span>
   </div>
 );
