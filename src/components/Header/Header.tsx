b57839572650c3a415232cf7f232872fabc57902
romasrs <shaman.roman@tut.by>
2021-05-20 21:54:38 +0300

    feat: implement burger menu, useBurger hook

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React, { SyntheticEvent, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import cn from 'classnames';
 import { useDispatch } from 'react-redux';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
 import styles from './Header.module.scss';
@@ -7,6 +8,7 @@ import defaultUserImage from '../assets/defaultPic.jpg';
 import { getProfile, logOut } from '../../redux/actions/user';
 import { Routes } from '../../helpers/constants';
 import LinkButton from '../Block/LinkButton';
+import useBurgerMenu from '../../hooks/useBurgerMenu';
 
 const Header: React.FC = () => {
   const { user, isLogged } = useTypedSelector((state) => state.user);
@@ -24,16 +26,31 @@ const Header: React.FC = () => {
     history.push(Routes.HOME);
   };
 
+  const { isOpen, setIsOpenBurger, wrapperRef } = useBurgerMenu();
+
   return (
-    <header>
+    <header ref={wrapperRef}>
       <div className={styles.title}>
         <h1>
           <Link to={Routes.HOME}>Realworld Blog</Link>
         </h1>
       </div>
 
+      <button
+        type="button"
+        className={cn(styles.burger, isOpen ? styles.open : '')}
+        onClick={() => setIsOpenBurger(!isOpen)}
+      >
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+      </button>
+
       {isLogged ? (
-        <>
+        <div className={cn(styles.navMenu, isOpen ? styles.open : '')}>
           <LinkButton to={Routes.NEW_ARTICLE} classname={['green', 'small']}>
             Create article
           </LinkButton>
@@ -44,16 +61,16 @@ const Header: React.FC = () => {
           <a href="" className={styles.logOut} onClick={(event) => logOutUser(event)}>
             Log Out
           </a>
-        </>
+        </div>
       ) : (
-        <>
+        <div className={cn(styles.navMenu, isOpen ? styles.open : '')}>
           <LinkButton to={Routes.SIGN_IN} classname={['signIn']}>
             Sign In
           </LinkButton>
           <LinkButton to={Routes.SIGN_UP} classname={['green']}>
             Sign Up
           </LinkButton>
-        </>
+        </div>
       )}
     </header>
   );
